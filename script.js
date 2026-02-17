53e1d95b4379d660e9d6020cb9d6eeddade16a3f
Paulo <paulopkj@gmail.com>
2026-02-17 19:16:48 -0300

    carros esportivos

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const user = {
+    nome: "Rodolfo",
+    idade: 30,
+    nomeDoConjuge: "Ana",
+    id: null
+}
+console.log(user);
+
+//-- SCRIPT SCROLL REVEAL --//
+
+   ScrollReveal().reveal(".reveal", {
+        distance: "50px",
+        duration: 1000,
+        easing: "ease-in-out",
+        origin: "bottom",
+        interval: 200,
+      });
